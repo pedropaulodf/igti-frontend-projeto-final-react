39775a08fb2c8bd0760eb870624005da0212d170
Pedro Paulo <pedropaulotj@gmail.com>
2020-11-13 19:04:11 -0300

    Ajustes no layout

--- src/App.js
+++ src/App.js
@@ -117,6 +117,75 @@ function App() {
             </div>
           </div>
         </div>
+        <div className="post">
+          <div className="postImg">
+            <img
+              src="https://image.tmdb.org/t/p/w500/A5TK9Q63r2h4cx1q2Isl3bTaVlY.jpg"
+              alt=""
+            />
+          </div>
+
+          <div className="postInfo">
+            <div className="postUserData">
+              <div className="postUserAvatar">
+                <img src={antMan} alt="" />
+              </div>
+              <div className="postUserName">
+                <p>
+                  <b>superman</b>
+                </p>
+              </div>
+              <div className="postUserComment">
+                <p>Gosto muito desse filme: "Aquaman".</p>
+              </div>
+            </div>
+
+            <div className="likesCounter">
+              <div className="likes">🖤 5</div>
+              <div className="comments">💬 3</div>
+            </div>
+
+            <div className="areaComments">
+              <div className="comment">
+                <div className="friendAvatar">
+                  <img src={antMan} alt="" />
+                </div>
+                <div className="friendName">
+                  <p>aquaman</p>
+                </div>
+                <div className="friendComment">
+                  <p>
+                    Vou parar de te seguir se continuar postando filmes assim...
+                  </p>
+                </div>
+              </div>
+
+              <div className="comment">
+                <div className="friendAvatar">
+                  <img src={antMan} alt="" />
+                </div>
+                <div className="friendName">
+                  <p>spiderMan</p>
+                </div>
+                <div className="friendComment">
+                  <p>Muito bom! Esse filme realmente é sensacional!</p>
+                </div>
+              </div>
+
+              <div className="inputCommentArea">
+                <div className="actualUserAvatar">
+                  <img src={antMan} alt="" />
+                </div>
+                <div className="inputArea">
+                  <input type="text" placeholder="Digite seu comentário..."/>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </div>
+
+
       </div>
     </div>
   );
